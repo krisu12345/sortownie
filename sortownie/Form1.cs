03ccd1a0905024d53a4a69fb4b7508ed4c95ca48
Visual Student <visualstudent@zschie.edu.pl>
2021-12-03 15:17:48 +0100

    dzialaja dwa sortowania

--- sortownie/Form1.cs
+++ sortownie/Form1.cs
@@ -107,8 +107,68 @@ namespace sortownie
                         Tablica.Add(int.Parse(line));
                     }
                 }
-            } }
-          
+            }
+            //ile razy i czas
+            decimal ilerazy = numericUpDown2.Value;
+            DateTime pomiar = DateTime.Now;
+            for (int i = 0; i < ilerazy; i++)
+            {
+                Quick_Sort(Tablica,0,Tablica.Count-1);
+            }
+            DateTime po = DateTime.Now;
+            TimeSpan czas = po - pomiar;
+            label4.Text = czas.Seconds.ToString() + "s";
 
         }
+        private static void Quick_Sort(List<int> arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                if (pivot > 1)
+                {
+                    Quick_Sort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    Quick_Sort(arr, pivot + 1, right);
+                }
+            }
+
+        }
+
+        private static int Partition(List<int> arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right]) return right;
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+
+
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
     }
